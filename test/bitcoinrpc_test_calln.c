9417025ed7afaaa789b19055317a1e9428c7392a
Marek Miller <marek.l.miller@gmail.com>
2016-02-20 09:42:29 +0100

    tu: _calln_getconnectioncount13

--- test/bitcoinrpc_test_calln.c
+++ test/bitcoinrpc_test_calln.c
@@ -33,13 +33,60 @@
 #include "bitcoinrpc_test.h"
 
 
-BITCOINRPC_TESTU(calln_help13)
+BITCOINRPC_TESTU(calln_getconnectioncount13)
 {
   BITCOINRPC_TESTU_INIT;
 
   const size_t n = 13;
-  (void) n;
 
+  bitcoinrpc_cl_t *cl = (bitcoinrpc_cl_t*)testdata;
+
+  bitcoinrpc_method_t *m[n];
+  bitcoinrpc_resp_t *r[n];
+  bitcoinrpc_err_t e;
+  json_t *j = NULL;
+
+
+  for (size_t i = 0; i < n; i++)
+    {
+      m[i] = bitcoinrpc_method_init(BITCOINRPC_METHOD_GETCONNECTIONCOUNT);
+      BITCOINRPC_ASSERT(m[i] != NULL,
+                        "cannot initialise a new method");
+
+      r[i] = bitcoinrpc_resp_init();
+      BITCOINRPC_ASSERT(r[i] != NULL,
+                        "cannot initialise a new response");
+    }
+
+  bitcoinrpc_calln(cl, n, m, r, &e);
+  BITCOINRPC_ASSERT(e.code == BITCOINRPCE_OK,
+                    "cannot perform a call");
+
+  for (size_t i = 0; i < n; i++)
+    {
+      j = bitcoinrpc_resp_get(r[i]);
+      BITCOINRPC_ASSERT(j != NULL,
+                        "cannot parse response from the server");
+
+      json_t *jerr = json_object_get(j, "error");
+      BITCOINRPC_ASSERT(json_equal(jerr, json_null()),
+                        "the server returned non zero error code");
+      json_decref(jerr);
+
+      json_t *jresult = json_object_get(j, "result");
+      BITCOINRPC_ASSERT(jresult != NULL,
+                        "the response has no key: \"result\"");
+
+      BITCOINRPC_ASSERT(json_is_integer(jresult),
+                        "getconnectioncount value is not an integer");
+      json_decref(j);
+    }
+
+  for (size_t i = 0; i < n; i++)
+    {
+      bitcoinrpc_resp_free(r[i]);
+      bitcoinrpc_method_free(m[i]);
+    }
 
   BITCOINRPC_TESTU_RETURN(0);
 }
@@ -60,7 +107,7 @@ BITCOINRPC_TESTU(calln)
 
 
   /* Perform test with the same client */
-  BITCOINRPC_RUN_TEST(calln_help13, o, cl);
+  BITCOINRPC_RUN_TEST(calln_getconnectioncount13, o, cl);
 
   bitcoinrpc_cl_free(cl);
   cl = NULL;
