a7a8712c67a3c480d43bfedbd18c0f603f8bd442
Marek Miller <marek.l.miller@gmail.com>
2016-02-21 16:38:04 +0100

    tu: calln_getconnectioncount27_settxfee41

--- test/bitcoinrpc_test_calln.c
+++ test/bitcoinrpc_test_calln.c
@@ -92,6 +92,101 @@ BITCOINRPC_TESTU(calln_getconnectioncount13)
 }
 
 
+BITCOINRPC_TESTU(calln_getconnectioncount27_settxfee41)
+{
+  BITCOINRPC_TESTU_INIT;
+
+  const size_t n1 = 27;
+  const size_t n2 = 41;
+  double fee = 0.001;
+  bitcoinrpc_cl_t *cl = (bitcoinrpc_cl_t*)testdata;
+
+  bitcoinrpc_method_t *m[n1 + n2];
+  bitcoinrpc_resp_t *r[n1 + n2];
+  bitcoinrpc_err_t e;
+  json_t *j = NULL;
+
+
+  for (size_t i = 0; i < n1; i++)
+    {
+      m[i] = bitcoinrpc_method_init(BITCOINRPC_METHOD_GETCONNECTIONCOUNT);
+      BITCOINRPC_ASSERT(m[i] != NULL,
+                        "cannot initialise a new method");
+
+      r[i] = bitcoinrpc_resp_init();
+      BITCOINRPC_ASSERT(r[i] != NULL,
+                        "cannot initialise a new response");
+    }
+
+  for (size_t i = n1; i < n1 + n2; i++)
+    {
+      json_t *jparams = json_array();
+      json_array_append_new(jparams, json_real(fee));
+      m[i] = bitcoinrpc_method_init_params(BITCOINRPC_METHOD_SETTXFEE, jparams);
+      fee += fee / 2;
+      json_decref(jparams);
+
+      BITCOINRPC_ASSERT(m[i] != NULL,
+                        "cannot initialise a new method");
+
+      r[i] = bitcoinrpc_resp_init();
+      BITCOINRPC_ASSERT(r[i] != NULL,
+                        "cannot initialise a new response");
+    }
+
+  bitcoinrpc_calln(cl, n1 + n2, m, r, &e);
+  BITCOINRPC_ASSERT(e.code == BITCOINRPCE_OK,
+                    "cannot perform a call");
+
+  for (size_t i = 0; i < n1; i++)
+    {
+      j = bitcoinrpc_resp_get(r[i]);
+      BITCOINRPC_ASSERT(j != NULL,
+                        "cannot parse response from the server");
+
+      json_t *jerr = json_object_get(j, "error");
+      BITCOINRPC_ASSERT(json_equal(jerr, json_null()),
+                        "the server returned non zero error code");
+      json_decref(jerr);
+
+      json_t *jresult = json_object_get(j, "result");
+      BITCOINRPC_ASSERT(jresult != NULL,
+                        "the response has no key: \"result\"");
+
+      BITCOINRPC_ASSERT(json_is_integer(jresult),
+                        "getconnectioncount value is not an integer");
+      json_decref(j);
+    }
+
+  for (size_t i = n1; i < n1 + n2; i++)
+    {
+      j = bitcoinrpc_resp_get(r[i]);
+      BITCOINRPC_ASSERT(j != NULL,
+                        "cannot parse response from the server");
+
+      json_t *jerr = json_object_get(j, "error");
+      BITCOINRPC_ASSERT(json_equal(jerr, json_null()),
+                        "the server returned non zero error code");
+      json_decref(jerr);
+
+      json_t *jresult = json_object_get(j, "result");
+      BITCOINRPC_ASSERT(jresult != NULL,
+                        "the response has no key: \"result\"");
+
+      BITCOINRPC_ASSERT(json_is_true(jresult),
+                        "cannot set new fee");
+      json_decref(j);
+    }
+
+  for (size_t i = 0; i < n1 + n2; i++)
+    {
+      bitcoinrpc_resp_free(r[i]);
+      bitcoinrpc_method_free(m[i]);
+    }
+
+  BITCOINRPC_TESTU_RETURN(0);
+}
+
 
 
 
@@ -108,6 +203,8 @@ BITCOINRPC_TESTU(calln)
 
   /* Perform test with the same client */
   BITCOINRPC_RUN_TEST(calln_getconnectioncount13, o, cl);
+  BITCOINRPC_RUN_TEST(calln_getconnectioncount27_settxfee41, o, cl);
+
 
   bitcoinrpc_cl_free(cl);
   cl = NULL;
