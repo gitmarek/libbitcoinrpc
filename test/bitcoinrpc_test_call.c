e695c0d730297a5011757f3bdf288263c07d1812
Marek Miller <marek.l.miller@gmail.com>
2016-02-23 08:09:51 +0100

    tu: generate0 and generate99

--- test/bitcoinrpc_test_call.c
+++ test/bitcoinrpc_test_call.c
@@ -430,7 +430,7 @@ BITCOINRPC_TESTU(call_getbalance_noparams)
                     "the response has no key: \"result\"");
 
   BITCOINRPC_ASSERT(json_is_real(jresult),
-                    "getinfo value is not an real number");
+                    "getbalance value is not an real number");
 
 
   json_decref(jparams);
@@ -464,7 +464,7 @@ BITCOINRPC_TESTU(call_getbalance_minconf10)
                     "cannot initialise a new response");
 
   bitcoinrpc_call(cl, m, r, &e);
-  
+
   BITCOINRPC_ASSERT(e.code == BITCOINRPCE_OK,
                     "cannot perform a call");
 
@@ -482,8 +482,121 @@ BITCOINRPC_TESTU(call_getbalance_minconf10)
                     "the response has no key: \"result\"");
 
   BITCOINRPC_ASSERT(json_is_real(jresult),
-                    "getinfo value is not an real number");
+                    "getbalance value is not an real number");
 
+
+  json_decref(jparams);
+  json_decref(j);
+  bitcoinrpc_resp_free(r);
+  bitcoinrpc_method_free(m);
+  BITCOINRPC_TESTU_RETURN(0);
+}
+
+
+
+BITCOINRPC_TESTU(call_generate0)
+{
+  BITCOINRPC_TESTU_INIT;
+
+  bitcoinrpc_cl_t *cl = (bitcoinrpc_cl_t*)testdata;
+  bitcoinrpc_method_t *m = NULL;
+  bitcoinrpc_resp_t *r = NULL;
+  bitcoinrpc_err_t e;
+  json_t *j = NULL;
+  json_t *jparams = NULL;
+
+  size_t n = 0;
+
+  jparams = json_array();
+  json_array_append_new(jparams, json_integer(n));
+
+  m = bitcoinrpc_method_init_params(BITCOINRPC_METHOD_GENERATE, jparams);
+  BITCOINRPC_ASSERT(m != NULL,
+                    "cannot initialise a new method");
+
+  r = bitcoinrpc_resp_init();
+  BITCOINRPC_ASSERT(r != NULL,
+                    "cannot initialise a new response");
+
+  bitcoinrpc_call(cl, m, r, &e);
+
+  BITCOINRPC_ASSERT(e.code == BITCOINRPCE_OK,
+                    "cannot perform a call");
+
+  j = bitcoinrpc_resp_get(r);
+  BITCOINRPC_ASSERT(j != NULL,
+                    "cannot parse response from the server");
+
+  json_t *jerr = json_object_get(j, "error");
+  BITCOINRPC_ASSERT(json_equal(jerr, json_null()),
+                    "the server returned non zero error code");
+  json_decref(jerr);
+
+  json_t *jresult = json_object_get(j, "result");
+  BITCOINRPC_ASSERT(jresult != NULL,
+                    "the response has no key: \"result\"");
+
+  BITCOINRPC_ASSERT(json_is_array(jresult),
+                    "generate method result is not an array");
+
+  BITCOINRPC_ASSERT(json_array_size(jresult) == n,
+                    "the array returned is of wrong size");
+
+  json_decref(jparams);
+  json_decref(j);
+  bitcoinrpc_resp_free(r);
+  bitcoinrpc_method_free(m);
+  BITCOINRPC_TESTU_RETURN(0);
+}
+
+
+BITCOINRPC_TESTU(call_generate99)
+{
+  BITCOINRPC_TESTU_INIT;
+
+  bitcoinrpc_cl_t *cl = (bitcoinrpc_cl_t*)testdata;
+  bitcoinrpc_method_t *m = NULL;
+  bitcoinrpc_resp_t *r = NULL;
+  bitcoinrpc_err_t e;
+  json_t *j = NULL;
+  json_t *jparams = NULL;
+
+  size_t n = 99;
+
+  jparams = json_array();
+  json_array_append_new(jparams, json_integer(n));
+
+  m = bitcoinrpc_method_init_params(BITCOINRPC_METHOD_GENERATE, jparams);
+  BITCOINRPC_ASSERT(m != NULL,
+                    "cannot initialise a new method");
+
+  r = bitcoinrpc_resp_init();
+  BITCOINRPC_ASSERT(r != NULL,
+                    "cannot initialise a new response");
+
+  bitcoinrpc_call(cl, m, r, &e);
+
+  BITCOINRPC_ASSERT(e.code == BITCOINRPCE_OK,
+                    "cannot perform a call");
+
+  j = bitcoinrpc_resp_get(r);
+  BITCOINRPC_ASSERT(j != NULL,
+                    "cannot parse response from the server");
+
+  json_t *jerr = json_object_get(j, "error");
+  BITCOINRPC_ASSERT(json_equal(jerr, json_null()),
+                    "the server returned non zero error code");
+  json_decref(jerr);
+
+  json_t *jresult = json_object_get(j, "result");
+  BITCOINRPC_ASSERT(jresult != NULL,
+                    "the response has no key: \"result\"");
+
+  BITCOINRPC_ASSERT(json_is_array(jresult),
+                    "generate method result is not an array");
+
+  BITCOINRPC_ASSERT(json_array_size(jresult) == n,
+                    "the array returned is of wrong size");
 
   json_decref(jparams);
   json_decref(j);
@@ -513,6 +626,8 @@ BITCOINRPC_TESTU(call)
   BITCOINRPC_RUN_TEST(call_settxfee47, o, cl);
   BITCOINRPC_RUN_TEST(call_getbalance_noparams, o, cl);
   BITCOINRPC_RUN_TEST(call_getbalance_minconf10, o, cl);
+  BITCOINRPC_RUN_TEST(call_generate0, o, cl);
+  BITCOINRPC_RUN_TEST(call_generate99, o, cl);
 
   bitcoinrpc_cl_free(cl);
   cl = NULL;
